d554b14f2698a2183f87894ea04544954fab1419
Ruri <rfigueroa@stu.jsu.edu>
2022-02-28 12:48:32 -0600

    Started working on it.

--- src/edu/jsu/mcis/cs310/tas_sp22/Badge.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/Badge.java
@@ -1,9 +1,30 @@
 package edu.jsu.mcis.cs310.tas_sp22;
 
-/**
- *
- * @author 
- */
 public class Badge {
+    private String description, badgeId;
+    
+    public Badge(String badgeId, String description)
+    {
+        this.badgeId = badgeId;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getBadgeId() {
+        return badgeId;
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder s = new StringBuilder();       
+        
+        s.append('#').append(badgeId).append(" ");
+        s.append('(').append(description).append(')');
+        
+        return s.toString();
+    }
     
 }
