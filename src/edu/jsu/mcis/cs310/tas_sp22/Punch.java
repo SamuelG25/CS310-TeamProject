f98732c2c422b29f31b7f2d2b102ec3c08cca246
Ruri <rfigueroa@stu.jsu.edu>
2022-03-04 13:25:02 -0600

    Update on Punch class and Created PunchType class

--- src/edu/jsu/mcis/cs310/tas_sp22/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/Punch.java
@@ -1,32 +1,28 @@
-
 package edu.jsu.mcis.cs310.tas_sp22;
 
 import java.security.Timestamp;
 import java.util.*;
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 
 public class Punch {
     private String badgeID;
     private int terminalID;
-    private int eventType;
-    private LocalTime originalTimeStamp;
+    private PunchType eventType;
+    private int ID;
+    private LocalDateTime originalTimeStamp;
     private LocalTime adjustedTimeStamp;
 
     public Punch(LinkedHashMap<String,String> param) {
         this.badgeID = ("badgeid");
         this.terminalID = Integer.parseInt("terminalid");
-        this.eventType = Integer.parseInt("eventtypeid");
-        this.originalTimeStamp = LocalTime.parse("timestamp");
+        this.eventType = PunchType.values()[Integer.parseInt("eventtypeid")];
+        this.originalTimeStamp = LocalDateTime.parse(("timestamp"),DateTimeFormatter.ofPattern("EEE MM-dd-yyyy HH:mm:ss"));
+        this.adjustedTimeStamp = null;
+        this.ID = 0;
     }
     
-    enum PunchType {
-    
-    CLOCK_OUT,
-    CLOCK_IN,
-    TIME_OUT;
-    }
-
     public String getBadgeID() {
         return badgeID;
     }
@@ -35,11 +31,15 @@ public class Punch {
         return terminalID;
     }
 
-    public int getEventType() {
+    public PunchType getEventType() {
         return eventType;
     }
+    
+    public int ID(){
+        return ID;
+    }
 
-    public LocalTime getOriginalTimeStamp() {
+    public LocalDateTime getOriginalTimeStamp() {
         return originalTimeStamp;
     }
 
@@ -47,6 +47,15 @@ public class Punch {
         return adjustedTimeStamp;
     }
     
-    
+    @Override
+    public String toString(){
+        StringBuilder s = new StringBuilder();
+        
+        s.append('#').append(badgeID);
+        s.append(" ").append(eventType);
+        s.append(": ").append(originalTimeStamp);
+
+        return s.toString();
+    }
     
 }
